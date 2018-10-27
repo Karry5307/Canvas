a54657fb3e2b4b4ba2672210bc59e2f206054a33
Karry5307 <39618630+Karry5307@users.noreply.github.com>
2018-10-27 14:15:11 +0800

    Change the indentation of main.js

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 var canvas = document.getElementById('canvas');
-		var ctx = canvas.getContext('2d');
-		var blockSize = 10;
-		function readColor() {
-			var colorString = prompt('Please input color: ').toUpperCase();
-			if(!(/[\dA-F]/.test(colorString))) {
-				alert('Input error!');
+var ctx = canvas.getContext('2d');
+var blockSize = 10;
+function readColor() {
+	var colorString = prompt('Please input color: ').toUpperCase();
+	if(!(/[\dA-F]/.test(colorString))) {
+		alert('Input error!');
+	} else {
+		ctx.fillStyle = "#" + colorString;
+	}
+}
+addEventListener("click",function(event) {
+		console.log(event.pageX, event.pageY);
+		var x = Math.floor(event.pageX / blockSize) * blockSize - 10;
+		var y = Math.floor(event.pageY / blockSize) * blockSize - 10;
+		ctx.fillRect(x, y, blockSize, blockSize);
+});
+addEventListener("keydown",function(event) {
+		if(event.keyCode == 86 && event.ctrlKey) {
+			readColor();
+		}
+});
+addEventListener("keydown",function(event) {
+		if(event.keyCode == 87 && event.ctrlKey) {
+			var size = Number(prompt("Please input size!"));
+			if(isNaN(size)) {
+				alert("Input error!");
 			} else {
-				ctx.fillStyle = "#" + colorString;
+				blockSize = size;
 			}
 		}
-		addEventListener("click",function(event) {
-				console.log(event.pageX, event.pageY);
-				var x = Math.floor(event.pageX / blockSize) * blockSize - 10;
-				var y = Math.floor(event.pageY / blockSize) * blockSize - 10;
-				ctx.fillRect(x, y, blockSize, blockSize);
-		});
-		addEventListener("keydown",function(event) {
-				if(event.keyCode == 86 && event.ctrlKey) {
-					readColor();
-				}
-		});
-		addEventListener("keydown",function(event) {
-				if(event.keyCode == 87 && event.ctrlKey) {
-					var size = Number(prompt("Please input size!"));
-					if(isNaN(size)) {
-						alert("Input error!");
-					} else {
-						blockSize = size;
-					}
-				}
-		});
+});
